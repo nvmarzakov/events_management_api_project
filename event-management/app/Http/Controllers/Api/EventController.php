c74c4ed968578e385913ec5a1719ec204f46492d
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2025-10-27 08:09:57 +0200

    Events - implement Update operation

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -45,9 +45,18 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time',
+        ]);
+
+        $event->update($validated);
+
+        return $event;
     }
 
     /**
